b1da31fc5f4644a34692cc9a9e4cb9391db3554b
Youssef Imzoughene <amzianx@gmail.com>
2019-04-12 23:20:11 +0100

    variables

---
+++ js/variables.js
@@ -0,0 +1,23 @@
+"use strict"
+//Variables
+var fullName = "Imzoughene Youssef";
+console.log(fullName);
+var me = "muslim",
+    job = "developer"
+console.log(me,job)
+
+//Global variable
+var message = "make a cookie"
+console.log(message)
+function sayGreeting(){
+    var message = "welcome world"
+    console.log(message)
+    //local variable ; message
+}
+sayGreeting();
+
+let x = 5;
+console.log(x);
+
+const RELIGION = "Islam"
+console.log(RELIGION)
