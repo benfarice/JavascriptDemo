58491dc705ab87a728e8f9fa2ae90facb7dbb831
Youssef Imzoughene <amzianx@gmail.com>
2019-07-30 20:06:34 +0100

    User interaction : alert..confirm .. prompt

---
+++ js/user_interaction.js
@@ -0,0 +1,11 @@
+//Browser specific functions
+
+//alert("this is a warning message");
+
+//var role = prompt("What is your role ?","developer")
+//console.log(role)
+
+//alert(`You are a ${role} ! good for you !`)
+
+var isMuslim = confirm("Are you muslim")
+alert(isMuslim)
