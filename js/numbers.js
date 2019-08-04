dfa58bd6dcb3aa7c46aa7d774c4b273b8058a05c
Youssef Imzoughene <amzianx@gmail.com>
2019-08-04 10:28:29 +0100

    Numbers

---
+++ js/numbers.js
@@ -0,0 +1,61 @@
+//Numbers
+/*
+var numberObj = new Number(50)
+console.log(numberObj.valueOf())
+
+var nombre = 1993;
+console.log(nombre.toString())
+console.log(nombre.toString(2))
+console.log(nombre.toString(16))
+
+console.log((255).toString(16))
+
+console.log("-------------------------------------------------")
+
+var numberObj = new Number(58);
+console.log(numberObj.valueOf())
+
+console.log("-------------------------------------------------")
+
+console.log(parseInt("699px"))
+console.log(parseFloat("48$"))
+
+console.log("-------------------------------------------------")
+
+console.log(Math.floor(12.87))
+console.log(Math.ceil(4.98))
+console.log(Math.round(34.87))
+console.log(Math.trunc(45.98))
+console.log(Math.random())
+
+console.log(Math.max(23,76,-8,-5,0))
+console.log(Math.min(3,5,6,8,98,09,-7))
+console.log(Math.pow(5,3))
+
+console.log("-------------------------------------------------")
+//toFixed
+var num = 56.987
+console.log(num.toFixed())
+console.log(num.toFixed(2))
+console.log(num.toFixed(6))
+
+//ToExponential
+var c = 456,y=500008;
+console.log(c.toExponential())
+console.log(y.toExponential())
+console.log("-------------------------------------------------")
+
+//ToPrecision
+var e = 567.9845
+console.log(e.toPrecision())
+console.log(e.toPrecision(2))
+console.log(e.toPrecision(4))
+*/
+
+//Difference between number objet nd primiive number
+var m=67,d=new Number(568);
+console.log(typeof m)
+console.log(typeof d)
+//======
+console.log(m instanceof Number)
+console.log(d instanceof Number)
